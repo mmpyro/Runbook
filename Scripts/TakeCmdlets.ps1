154d12a297d6efa208d0fd394804d948bdc3df36
mmpyro <mmpyro@github.com>
2019-05-19 16:46:38 +0200

    RunbookModule.psd1 was replaced by Runbook.psd1

--- Scripts/TakeCmdlets.ps1
+++ Scripts/TakeCmdlets.ps1
@@ -32,6 +32,7 @@ Get-ChildItem -Path "$Src\Cmdlets" -Filter '*Cmdlet.cs'| % {
 
 
 Copy-Item -Path "$Src\bin\$Configuration\*" -Recurse
+$assemblies = Get-ChildItem -Path $Dst -Filter "*.dll"|Select -ExpandProperty Name
 
-New-ModuleManifest -Path "RunbookModule.psd1" -Author 'mmpyro' -RootModule "RunbookModule.dll" `
- -CmdletsToExport $cmdlets -ProjectUri $uri -DotNetFrameworkVersion $dotnetVersion -PowerShellVersion $psVersion
+New-ModuleManifest -Path "Runbook.psd1" -Author 'mmpyro' -RootModule "RunbookModule.dll" `
+ -CmdletsToExport $cmdlets -ProjectUri $uri -DotNetFrameworkVersion $dotnetVersion -PowerShellVersion $psVersion -RequiredAssemblies $assemblies
